922060fbbb3d9e37609524957e6e7713101e8b5f
jbirchw1 <jbirchw1@binghamton.edu>
2025-10-08 14:04:57 -0400

    switched away from nginx for server rendering and back to SvelteKit native ssr. Adjusted docker-compose accordingly

--- src/frontend-2/src/routes/create-account/+page.svelte
+++ src/frontend-2/src/routes/create-account/+page.svelte
@@ -1,3 +1,19 @@
+<script>
+  export let form;
+</script>
 
+<h1>Sign Up</h1>
 
-<h1>Create an Account</h1>
+<form method="POST" action="?/signUp">
+  <input type="text" name="email" placeholder="Email">
+  <input type="password" name="password" placeholder="Password">
+  <button type="submit">Sign Up</button>
+
+  {#if form?.missing}
+    <p style="color:red">Please fill in all fields.</p>
+  {/if}
+
+  {#if form?.success}
+    <p style="color:green">Account created!</p>
+  {/if}
+</form>
