d5a699eabff36fcebc391d736c27daf1fc135cf9
jbirchw1 <jbirchw1@binghamton.edu>
2025-10-13 12:43:17 -0400

    api call from svelte fronted

--- src/frontend-2/src/routes/create-account/+page.server.js
+++ src/frontend-2/src/routes/create-account/+page.server.js
@@ -2,6 +2,8 @@ import { fail } from '@sveltejs/kit'
 
 export const actions = {
   signUp: async ({ request }) => {
+    const apiUrl = process.env.API_URL || 'http://localhost:8080'; // Fall back to 8080 if API_URL doesn't exist in env
+
     const formData = await request.formData()
 
     const email = String(formData.get('email'))
@@ -10,11 +12,24 @@ export const actions = {
     if (!email || !password) {
       return fail(400, { email, password, missing: true })
     }
-
-    // TODO: API call goes here
+    
     console.log("Creating ", email, ", ", password);
 
-    return { success: true }
+    // Call to the API
+    const res = await fetch(`${apiUrl}/status`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(formData),
+    });
+
+    if (!res.ok) {
+    return new Response(JSON.stringify({ error: 'API request failed' }), { status: 500 });
+    }
+
+    const data = await res.json();
+    return new Response(JSON.stringify(data), { status: 200 });
   }
   // more actions can go here -- e.g. forgot password?
 }
