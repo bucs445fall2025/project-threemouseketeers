1fdb5153d83867ec4e4300fa00a5ecbc40ca99ab
jbirchw1 <jbirchw1@binghamton.edu>
2025-10-20 11:33:44 -0400

    update page with response from api -- weird status returned?

--- src/frontend-2/src/routes/create-account/+page.svelte
+++ src/frontend-2/src/routes/create-account/+page.svelte
@@ -1,19 +1,35 @@
 <script>
-  export let form;
+	let message = '';
+	let color = '';
+	
+	async function handleSubmit(event) {
+		event.preventDefault();
+		const formData = new FormData(event.target);
+
+		const res = await fetch('?/signUp', {
+			method: 'POST',
+			body: formData
+		});
+
+		if (res.status === 500) {
+			message = 'Account created!';
+			color = 'green';
+		} else if (res.status === 201) {
+			message = 'Please fill in all fields.';
+			color = 'red';
+		} else {
+			message = `Unexpected response (${res.status})`;
+			color = 'orange';
+		}
+	}
 </script>
 
-<h1>Sign Up</h1>
-
-<form method="POST" action="?/signUp">
-  <input type="text" name="email" placeholder="Email">
-  <input type="password" name="password" placeholder="Password">
-  <button type="submit">Sign Up</button>
-
-  {#if form?.missing}
-    <p style="color:red">Please fill in all fields.</p>
-  {/if}
-
-  {#if form?.success}
-    <p style="color:green">Account created!</p>
-  {/if}
+<form on:submit={handleSubmit}>
+	<input type="text" name="email" placeholder="Email" />
+	<input type="password" name="password" placeholder="Password" />
+	<button type="submit">Sign Up</button>
 </form>
+
+{#if message}
+<p style="color:{color}">{message}</p>
+{/if}
