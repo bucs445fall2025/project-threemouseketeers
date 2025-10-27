fdcbcc1b981be2485b70b815eac52d8eff50ba60
jbirchw1 <jbirchw1@binghamton.edu>
2025-10-27 12:06:03 -0400

    frontend changes

--- src/frontend-2/src/routes/+layout.svelte
+++ src/frontend-2/src/routes/+layout.svelte
@@ -9,4 +9,20 @@
 	<link rel="icon" href={favicon} />
 </svelte:head>
 
+<nav class="navbar">
+	<ul class="nav-links">
+    <li><a href='./create-account'>Sign Up</a></li>
+    <li>Already have an account?<a href='./log-in'>Log In</a></li>
+	</ul>
+</nav>
+
 {@render children?.()}
+
+<style>
+  .navbar {
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+		padding: 1rem 2rem;
+	}
+</style>
--- src/frontend-2/src/routes/+page.svelte
+++ src/frontend-2/src/routes/+page.svelte
@@ -1,5 +1,1 @@
-<h2><a href="/create-account">Sign Up</a></h2>
-<h2>Already have an account? <a href="/log-in">Log In</a></h2>
-
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<h1>Welcome to VisitU</h1>
