3f5a5840871e2344e5b649f12a0e5d5211b0125e
SenihOk <senih.okuyucu@gmail.com>
2025-10-27 11:58:53 -0400

    implementing express-mysql-session for user states across pages in website

--- src/frontend-2/src/routes/+layout.server.js
+++ src/frontend-2/src/routes/+layout.server.js
@@ -1,4 +1,13 @@
-export const load = ({ cookies }) => {
-  const token = cookies.get('jwt');
-  return { token };
-};
+// export const load = ({ cookies }) => {
+//   const token = cookies.get('jwt');
+//   return { token };
+// };
+
+import { hydrateUser, user } from "$lib/stores/auth";
+
+export async function load() {
+  await hydrateUser(); // populates store if session cookie valid
+  let current;
+  user.subscribe(value => current = value)();
+  return {user: current};
+}
