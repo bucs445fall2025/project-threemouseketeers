555762e104b800869a0ec4cf72ca5258e271defa
jbirchw1 <jbirchw1@binghamton.edu>
2025-10-27 13:16:59 -0400

    navbar

--- src/frontend-2/src/routes/+layout.svelte
+++ src/frontend-2/src/routes/+layout.svelte
@@ -3,6 +3,8 @@
 	import favicon from '$lib/assets/favicon.svg';
 
 	let { children } = $props();
+
+  import { page } from '$app/stores'; // This gets info about the URL
 </script>
 
 <svelte:head>
@@ -11,9 +13,23 @@
 
 <nav class="navbar">
 	<ul class="nav-links">
-    <li><a href='./create-account'>Sign Up</a></li>
-    <li>Already have an account?<a href='./log-in'>Log In</a></li>
+    <li><a href='./'>Home</a></li>
 	</ul>
+  <div class="user-icon">
+    {#if $page.url.pathname === '/log-in'}
+      <!-- show Sign Up when on login page -->
+      Don't have an account? <a href="/create-account">Sign Up</a>
+
+    {:else if $page.url.pathname === '/create-account'}
+      <!-- show Log In when on signup page -->
+      Already have an account? <a href="/log-in">Log In</a>
+
+    {:else}
+      <!-- show both for all other pages -->
+      <a href="/log-in">Log In</a>
+      <a href="/create-account">Sign Up</a>
+    {/if}
+  </div>
 </nav>
 
 {@render children?.()}
@@ -25,4 +41,18 @@
 		align-items: center;
 		padding: 1rem 2rem;
 	}
+
+  ul {
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+    overflow: hidden;
+  }
+
+  .user-icon {
+    margin-left: auto; 
+    margin-right: 0;
+    display: block;
+  }
+
 </style>
